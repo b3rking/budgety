7a003dd4246ac7cf90008872768ef95eed51b8c4
b3rking <ishimweeschylebersy@gmail.com>
2021-05-31 10:32:59 +0200

    reading the input data

--- app.js
+++ app.js
@@ -7,16 +7,33 @@ var budgetController = (function() {
 
 // ui controller
 var uiController = (function() {
-
-    // some code
+    var domStrings = {
+        type: '.add__type',
+        description: '.add__description',
+        value: '.add__value',
+        add: '.add__btn'
+    }
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(domStrings.type).value, // inc or exp
+                descr: document.querySelector(domStrings.description).value,
+                value: document.querySelector(domStrings.value).value
+            };
+        },
+        domStrings
+    };
 
 })();
 
 // global app controller
 var controller = (function() {
 
+    var dom = uiController.domStrings; // contain all strings for the selectors!!!!
+
     var ctrlAddItem = function() {
 
+        console.log(uiController.getInput());
         // 1. get the field input data
         // 2. add the item to the budget controller
         // 3. add the item to the ui
@@ -25,7 +42,7 @@ var controller = (function() {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(dom.add).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(e){
         if (e.keyCode === 13 || e.which === 13) {
